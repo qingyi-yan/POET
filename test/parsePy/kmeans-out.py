e7cd87009fe214e7cf94a47272d48438dfd4a173
qingyi2 <30488325+qingyi2@users.noreply.github.com>
2023-09-14 21:50:31 -0700

    Rename TypeDefParse to TypeDef in lib/Cfront.code.

--- test/parsePy/kmeans-out.py
+++ test/parsePy/kmeans-out.py
@@ -2,14 +2,14 @@ import numpy as np
 def kmeans(input_data,K):
   newc=input_data[:K]
   cluster={  }
-  centroids=np.zeros(K)
+  centroids=(np.zeros(K))
   while not(np.isclose(newc,centroids)).all() : 
     centroids=newc
-    sigma=np.zeros(K)
-    count=np.zeros(K)
+    sigma=(np.zeros(K))
+    count=(np.zeros(K))
     for x in input_data : 
-      t=np.abs(x-centroids)
-      i=np.where(t==t.min())[0][0]
+      t=(np.abs(x-centroids))
+      i=np.where(t==(t.min()))[0][0]
       cluster[x]=i
       sigma[i]+=x
       count[i]+=1 
@@ -18,10 +18,10 @@ def kmeans(input_data,K):
     newc=(sigma/count) 
   
    
-  return(centroids,cluster)
+  return centroids,cluster
 
 
-input_data=np.array([ 3,5,8,0,135,234,29 ],np.float)
+input_data=(np.array([ 3,5,8,0,135,234,29 ],(np.float)))
 centroids,cluster=kmeans(input_data,5)
 print "input:",input_data
 print centroids
